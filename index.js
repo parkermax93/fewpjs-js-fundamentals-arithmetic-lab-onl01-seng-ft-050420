3126acd6e70f0b9a57494316783079e758407679
Parker Williamson <parker.williamson@gmail.com>
2020-08-19 20:11:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Write your code here
 
-const num1 = 31
-const num2 = 2 
+const num1 = 31;
+const num2 = 2 ;
 
-const multiply = num1 * num2
+const multiply = num1 * num2;
 
-let random = Math.floor( math.random() * 1000) + 1
+let random = Math.floor( math.random() * 1000) + 1;
 
-const mod = 16 % 4 
+const mod = 16 % 4 ;
