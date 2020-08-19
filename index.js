89e8edc2b6541ea2e2cd3399b4f30638c85f43e6
Parker Williamson <parker.williamson@gmail.com>
2020-08-19 20:15:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+
+const num1 = 31;
+const num2 = 2 ;
+
+const multiply = num1 * num2;
+
+var random = Math.ceil(Math.random() * 10 + 1 );
+
+var mod = 14 % 5;
+
+var max = Math.max(1,2,3,4,20)
